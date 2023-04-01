c5c259cabcf9165cb1cba988ca5aca249132cb38
0xnbx <nick@0xnbx.com>
2023-04-01 13:34:29 -0700

    wip

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function run(): Promise<void> {
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
-  } catch (error) {
+  } catch (error: any) {
     core.setFailed(error.message);
   }
 }
