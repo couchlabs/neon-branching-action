35df1a5270eaed7d73a8464a9882e0bd63663cfd
0xnbx <nick@0xnbx.com>
2023-04-02 11:37:40 -0700

    wip fetch

--- src/main.ts
+++ src/main.ts
@@ -56,13 +56,14 @@ async function run(): Promise<void> {
         );
       }
 
+      console.log("newBranch", JSON.stringify(newBranch, undefined, 2));
       const [{ branch }, { endpoint }] = await Promise.all([
         getBranch(newBranch.id),
         getEndpoint(newEndpoint.endpoint_id!),
       ]);
 
-      console.log("branch-details", JSON.stringify(branch, undefined, 2));
-      console.log("endpoint-details", JSON.stringify(endpoint, undefined, 2));
+      console.log("branch-----details", JSON.stringify(branch, undefined, 2));
+      console.log("endpoint---details", JSON.stringify(endpoint, undefined, 2));
 
       core.setOutput("host_url", endpoint.host);
       core.setOutput("host_id", endpoint.id);
